847b216096d9a8ab6688b70a16e92e2b7a6ed866
Manti Mokone <mokonemanti256@gmail.com>
2026-07-12 13:10:52 -0400

    Add: validate-parentheses - submission-12

---
+++ Data Structures & Algorithms/validate-parentheses/submission-12.py
@@ -0,0 +1,24 @@
+from collections import deque
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        stack = deque()
+
+        
+
+        for br in s:
+            if br == '(' or br == "[" or br =="{":
+                stack.append(br)
+
+            elif br == "}" or br == "]" or br == ")":
+                print("CLoseing")
+                if not stack:
+                    return False
+
+                elif (br == "}" and stack[-1] != "{") or (br == "]" and stack[-1] != "[") or (br == ")" and stack[-1] != "("):
+                    return False
+                stack.pop()
+
+        
+        return not stack
+        
