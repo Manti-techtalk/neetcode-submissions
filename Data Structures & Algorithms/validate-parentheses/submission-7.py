c02444a4298fee28a2dd2d7cad58a612b3a07885
Manti Mokone <mokonemanti256@gmail.com>
2026-07-12 10:44:18 -0400

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.py
@@ -0,0 +1,29 @@
+from collections import deque
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = deque()
+        # lets add the opening brackets to the stack
+        c = 0
+        for br in s:
+            if br == "(" or br == "{" or br == "[":
+
+                stack.append(br)
+                c += 1
+
+        #look for closing brackets
+        print("Closing",stack)
+
+        for br in s:
+            if br == ")":
+                if "(" in stack:
+                    c -= 1
+
+            elif br == "]":
+                if "[" in stack:
+                    c -= 1
+            elif br == "}":
+                if "{" in stack:
+                    c-= 1
+
+        print("Finale", stack)
+        return c == 0
